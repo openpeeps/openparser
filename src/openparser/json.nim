6ab001582335916e18f86d2da996ed1c43063517
George Lemon <georgelemon@protonmail.com>
2026-05-10 00:22:07 +0300

    update
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- src/openparser/json.nim
+++ src/openparser/json.nim
@@ -156,64 +156,6 @@ proc isMapped*(m: MemFile): bool {.inline.} =
   ## True when this MemFile currently has a valid mapped region.
   m.mem != nil and m.size > 0
 
-
-#
-# JSONY object variants utility macros
-# https://github.com/treeform/jsony
-#
-proc hasKind(node: NimNode, kind: NimNodeKind): bool =
-  for c in node.children:
-    if c.kind == kind:
-      return true
-  return false
-
-proc `[]`(node: NimNode, kind: NimNodeKind): NimNode =
-  for c in node.children:
-    if c.kind == kind:
-      return c
-  return nil
-
-template fieldPairs[T: ref object](x: T): untyped =
-  x[].fieldPairs
-
-macro isObjectVariant(v: typed): bool =
-  # Is this an object variant?
-  var typ = v.getTypeImpl()
-  if typ.kind == nnkSym:
-    return ident("false")
-  while typ.kind != nnkObjectTy:
-    typ = typ[0].getTypeImpl()
-  if typ[2].hasKind(nnkRecCase):
-    ident("true")
-  else:
-    ident("false")
-
-proc discriminator(v: NimNode): NimNode =
-  var typ = v.getTypeImpl()
-  while typ.kind != nnkObjectTy:
-    typ = typ[0].getTypeImpl()
-  return typ[nnkRecList][nnkRecCase][nnkIdentDefs][nnkSym]
-
-macro discriminatorFieldName(v: typed): untyped =
-  # Turns into the discriminator field.
-  return newLit($discriminator(v))
-
-macro discriminatorField(v: typed): untyped =
-  # Turns into the discriminator field.
-  let
-    fieldName = discriminator(v)
-  return quote do:
-    `v`.`fieldName`
-
-macro new(v: typed, d: typed): untyped =
-  # Creates a new object variant with the discriminator field.
-  let
-    typ = v.getTypeInst()
-    fieldName = discriminator(v)
-  return quote do:
-    `v` = `typ`(`fieldName`: `d`)
-
-
 # Forward declarations
 proc objectToJson*(v, valImpl: NimNode, opts: JsonOptions = nil): NimNode
 proc arrayToJson*(v, valImpl: NimNode, opts: JsonOptions = nil): NimNode
