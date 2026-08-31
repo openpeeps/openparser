157f2d42e4da674d0fccaf53e3c7ae0f24667dad
George Lemon <georgelemon@protonmail.com>
2026-08-31 21:00:14 +0300

    build docs - discover new modules
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- src/openparser.nim
+++ src/openparser.nim
@@ -8,8 +8,10 @@ when defined(napibuild):
   discard
 elif defined(builddocs):
   # For documentation purposes, we re-export all parsers here
-  import ./openparser/[json, csv, rss, feed, yaml, dotenv, fbe, toml, bson, xml, nif, ical, plist]
-  export json, csv, rss, feed, yaml, dotenv, fbe, toml, bson, xml, nif, ical, plist
+  import ./openparser/[json, csv, rss, feed, yaml, dotenv,
+                    fbe, toml, bson, xml, nif, ical, plist, css, svg]
+  export json, csv, rss, feed, yaml, dotenv,
+      fbe, toml, bson, xml, nif, ical, plist, css, svg
 
   import ./openparser/gettext/[po, mo]
   export po, mo
